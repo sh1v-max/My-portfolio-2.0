cbd8f266d01fbd293df843223041dc9ef322c7eb
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-15 23:21:56 +0530

    updated article

--- src/features/articles/ArticleCard.jsx
+++ src/features/articles/ArticleCard.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable react/prop-types */
 function ArticleCard({ url, image, title, desc }) {
   return (
+    <a
+      href={url}
+      className="flex  max-w-xs cursor-pointer flex-col rounded-xl bg-articleBg  hover:shadow-lg"
+      target="_blank"
+      rel="noreferrer"
+    >
+    </a>
   );
 }
 
